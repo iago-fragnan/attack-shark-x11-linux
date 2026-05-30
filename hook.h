764670ec7dc388199cba794f51c28395bdc076da
Iago F. <n3rdydzn@hotmail.com>
2026-05-30 12:14:04 -0300

    feat: apply settings function

--- hook.h
+++ hook.h
@@ -2,14 +2,10 @@
 #define HOOK_H
 
 #include <QString>
+#include <QList>
+#include <QPair>
 
-class x11mouse
-{
-public:
-    x11mouse();
-};
-
-int setConfig(QString sel_deviceID, int sel_colormode, int sel_prate);
+int setConfig(QString devicePath, int colorMode, int pollingRate);
 int getBatteryInfo();
 QList<QPair<QString, QString>> getDevices(bool allDevices);
 
